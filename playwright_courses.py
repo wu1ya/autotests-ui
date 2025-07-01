3146b7855d63908b9ec0d6be9a8703b0ebd665c8
wu1ya <wu1yawrkk@gmail.com>
2025-07-01 19:06:52 +0500

    Работа с авторизацией в Playwright. Практика работы с авторизацией в Playwright.

--- playwright_courses.py
+++ playwright_courses.py
@@ -23,11 +23,18 @@ with sync_playwright() as playwright:
     page = context.new_page()
 
     page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses')
-    page.pause()
+
     section_title = page.get_by_test_id('courses-list-toolbar-title-text')
     expect(section_title).to_be_visible()
     expect(section_title).to_have_text('Courses')
 
-    empty_state_message = page.get_by_test_id('courses-list-empty-view-title-text')
-    expect(empty_state_message).to_be_visible()
-    expect(empty_state_message).to_have_text('There is no results')
+    empty_view_icon = page.get_by_test_id('courses-list-empty-view-icon')
+    expect(empty_view_icon).to_be_visible()
+
+    empty_state_title = page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(empty_state_title).to_be_visible()
+    expect(empty_state_title).to_have_text('There is no results')
+
+    empty_state_description = page.get_by_test_id('courses-list-empty-view-description-text')
+    expect(empty_state_description).to_be_visible()
+    expect(empty_state_description).to_have_text('Results from the load test pipeline will be displayed here')
